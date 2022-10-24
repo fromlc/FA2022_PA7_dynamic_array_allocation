e645fc6972d8bb471a5d2a665156bd65ce9d97fe
L Carver <wifihum@gmail.com>
2022-10-23 17:43:35 -0700

    split class RandomArray declaration and definition, add file RandomArray.cpp

---
+++ source/RandomArray.cpp
@@ -0,0 +1,153 @@
+//------------------------------------------------------------------------------
+// RandomArray.cpp : class definition
+//------------------------------------------------------------------------------
+#include <cstdlib>		// rand()
+#include <cstring>		// memset()
+#include <ctime>		// srand(time(0))
+#include <vector>		// for data
+
+#include "RandomArray.h"
+
+//------------------------------------------------------------------------------
+// using symbols
+//------------------------------------------------------------------------------
+using std::ostream;
+using std::string;
+using std::vector;
+
+//----------------------------------------------------------------------------
+// constructors
+//----------------------------------------------------------------------------
+RandomArray::RandomArray() : RandomArray(DEFAULT_ELEMENTS, MAX_ELEMENT_VALUE) {}
+
+//----------------------------------------------------------------------------
+RandomArray::RandomArray(int nElements, int xMax) {
+
+	m_numElements = nElements;
+	m_vE.reserve(nElements);
+
+	m_elementMax = xMax;
+	m_pCounts = new int[xMax + 1];		// values range from 0 to xMax
+
+	m_mode = -1;
+	m_modeOccurs = 0;
+
+	// seed random number generator
+	srand((unsigned int)time(0));
+
+	// fill m_pElements array with random numbers between 0 and m_elementMax
+	fillRandomArray();
+}
+
+//----------------------------------------------------------------------------
+// destructor
+//----------------------------------------------------------------------------
+RandomArray::~RandomArray() {
+	delete[] m_pCounts;
+}
+
+//----------------------------------------------------------------------------
+// getters
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+int RandomArray::getElementMax() { return m_elementMax; }
+
+//----------------------------------------------------------------------------
+int RandomArray::getNumElements() { return m_numElements; }
+
+//----------------------------------------------------------------------------
+// -calculates a single mode #TODO some data sets have two modes
+// -updates reference parameters to mode and how many times the mode occurred
+// -if no element occurred more than once return false, otherwise return true
+//----------------------------------------------------------------------------
+bool RandomArray::getMode(int& mode, int& modeOccurs) {
+	// number of times array mode occurs
+	m_modeOccurs = 0;
+
+	// calculate mode
+	for (auto e : m_vE) {
+		//----------------------------------------------------------------------------
+		// e is the number of movies that student #i watched
+		// 
+		// -bump count of students that watched e movies
+		// -find the highest count of students watching the same number of movies
+		// -the corresponding number of movies watched is the mode
+		//----------------------------------------------------------------------------
+
+		if (++(m_pCounts[e]) > m_modeOccurs) {
+			m_modeOccurs = m_pCounts[e];
+			m_mode = e;
+		}
+	}
+
+	// update reference parameters
+	mode = m_mode;
+	modeOccurs = m_modeOccurs;
+
+	return (modeOccurs < 2) ? false : true;
+}
+
+//----------------------------------------------------------------------------
+// calculates arithmetic mean of the array
+//----------------------------------------------------------------------------
+float RandomArray::getMean() {
+	// accumulator
+	int total = 0;
+
+	// accumulate total and calculate mean
+	for (auto e : m_vE) {
+		total += e;
+	}
+
+	m_mean = (float)total / (float)m_numElements;
+	return m_mean;
+}
+
+//----------------------------------------------------------------------------
+// setters
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+// use passed strings for output with overloaded operator << 
+//----------------------------------------------------------------------------
+void RandomArray::setOutputStrings(const string& s1,
+								   const string& s2,
+								   const string& s3) {
+	m_s1 = s1;
+	m_s2 = s2;
+	m_s3 = s3;
+}
+
+//------------------------------------------------------------------------------
+// private util
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// fill array with random numbers between 0 and the max element value
+//------------------------------------------------------------------------------
+void RandomArray::fillRandomArray() {
+	int max = m_elementMax + 1;
+
+	// get rid of any old vector elements
+	m_vE.clear();
+
+	// initialize vector of data values
+	for (int i = 0; i < m_numElements; i++) {
+		m_vE.push_back(rand() % max);
+	}
+
+	// zero int array memory for counting element data occurrence counts
+	memset(m_pCounts, 0, max * sizeof(int));
+}
+
+//----------------------------------------------------------------------------
+// overload operator <<
+//----------------------------------------------------------------------------
+ostream& operator<<(ostream& os, const RandomArray& ra) {
+	int i = 0;
+	for (auto e : ra.m_vE) {
+		os << ra.m_s1 << ++i << ra.m_s2 << e << ra.m_s3 << '\n';
+	}
+	return os;
+}
--- source/RandomArray.h
+++ source/RandomArray.h
@@ -1,12 +1,10 @@
 //------------------------------------------------------------------------------
-// RandomArray.h : class declaration and definition
+// RandomArray.h : class declaration
 //------------------------------------------------------------------------------
 #ifndef RANDOMARRAY_H
 #define RANDOMARRAY_H
 
-#include <cstdlib>		// rand()
-#include <cstring>		// memset()
-#include <ctime>		// srand(time(0))
+#include <iostream>		// ostream
 #include <vector>		// for data
 
 //------------------------------------------------------------------------------
@@ -19,10 +17,10 @@ using std::vector;
 //------------------------------------------------------------------------------
 // constants
 //------------------------------------------------------------------------------
-static constexpr int DEFAULT_ELEMENTS = 100;
-static constexpr int MAX_ELEMENTS = 100;
-static constexpr int MAX_ELEMENT_VALUE = 10;
-static constexpr int NO_MODE_EXISTS = -1;
+constexpr int DEFAULT_ELEMENTS = 100;
+constexpr int MAX_ELEMENTS = 100;
+constexpr int MAX_ELEMENT_VALUE = 10;
+constexpr int NO_MODE_EXISTS = -1;
 
 //------------------------------------------------------------------------------
 // RandomArray
@@ -48,92 +46,30 @@ public:
 	//----------------------------------------------------------------------------
 	// constructors
 	//----------------------------------------------------------------------------
-	RandomArray() : RandomArray(DEFAULT_ELEMENTS, MAX_ELEMENT_VALUE) {}
-
-	//----------------------------------------------------------------------------
-	RandomArray(int nElements, int xMax) {
-
-		m_numElements = nElements;
-		m_vE.reserve(nElements);
-
-		m_elementMax = xMax;
-		m_pCounts = new int[xMax + 1];		// values range from 0 to xMax
-
-		m_mode = -1;
-		m_modeOccurs = 0;
-
-		// seed random number generator
-		srand((unsigned int) time(0));
-
-		// fill m_pElements array with random numbers between 0 and m_elementMax
-		fillRandomArray();
-	}
+	RandomArray();
+	RandomArray(int nElements, int xMax);
 
 	//----------------------------------------------------------------------------
 	// destructor
 	//----------------------------------------------------------------------------
-	~RandomArray() {
-		delete[] m_pCounts;
-	}
+	~RandomArray();
 
 	//----------------------------------------------------------------------------
 	// getters
 	//----------------------------------------------------------------------------
-
-	//----------------------------------------------------------------------------
-	int getElementMax() { return m_elementMax; }
-
-	//----------------------------------------------------------------------------
-	int getNumElements() { return m_numElements; }
+	int getElementMax();
+	int getNumElements();
 
 	//----------------------------------------------------------------------------
 	// -calculates a single mode #TODO some data sets have two modes
 	// -updates reference parameters to mode and how many times the mode occurred
 	// -if no element occurred more than once return false, otherwise return true
 	//----------------------------------------------------------------------------
-	bool getMode(int& mode, int& modeOccurs) {
-		// number of times array mode occurs
-		m_modeOccurs = 0;
-
-		// calculate mode
-		for (auto e : m_vE) {
-
-			// e is the number of movies that student #i watched
-
-			//----------------------------------------------------------------------------
-			// -bump count of students that watched e movies
-			// -find the highest count of students watching the same number of movies
-			// -the corresponding number of movies watched is the mode
-			//----------------------------------------------------------------------------
-
-			if (++(m_pCounts[e]) > m_modeOccurs) {
-				m_modeOccurs = m_pCounts[e];
-				m_mode = e;
-			}
-		}
-
-		// update reference parameters
-		mode = m_mode;
-		modeOccurs = m_modeOccurs;
-
-		return (modeOccurs < 2) ? false : true;
-	}
-
+	bool getMode(int& mode, int& modeOccurs);
 	//----------------------------------------------------------------------------
 	// calculates arithmetic mean of the array
 	//----------------------------------------------------------------------------
-	float getMean() {
-		// accumulator
-		int total = 0;
-
-		// accumulate total and calculate mean
-		for (auto e : m_vE) {
-			total += e;
-		}
-
-		m_mean = (float)total / (float)m_numElements;
-		return m_mean;
-	}
+	float getMean();
 
 	//----------------------------------------------------------------------------
 	// setters
@@ -142,46 +78,16 @@ public:
 	//----------------------------------------------------------------------------
 	// use passed strings for output with overloaded operator << 
 	//----------------------------------------------------------------------------
-	void setOutputStrings(const string&& s1,
-						  const string&& s2, 
-						  const string&& s3) {
-		m_s1 = s1;
-		m_s2 = s2;
-		m_s3 = s3;
-	}
+	void setOutputStrings(const string& s1, const string& s2, const string& s3);
 
 private:
 	//------------------------------------------------------------------------------
-	// util
-	//------------------------------------------------------------------------------
-
-	//------------------------------------------------------------------------------
 	// fill array with random numbers between 0 and the max element value
 	//------------------------------------------------------------------------------
-	void fillRandomArray() {
-		int max = m_elementMax + 1;
-
-		// get rid of any old vector elements
-		m_vE.clear();
-
-		// initialize vector of data values
-		for (int i = 0; i < m_numElements; i++) {
-			m_vE.push_back(rand() % max);
-		}
-
-		// zero int array memory for counting element data occurrence counts
-		memset(m_pCounts, 0, max * sizeof(int));
-	}
-
+	void fillRandomArray();
 	//----------------------------------------------------------------------------
 	// overload operator <<
 	//----------------------------------------------------------------------------
-	friend ostream& operator<<(ostream& os, const RandomArray& ra) {
-		int i = 0;
-		for (auto e : ra.m_vE) {
-			os << ra.m_s1 << ++i << ra.m_s2 << e << ra.m_s3 << '\n';
-		}
-		return os;
-	}
+	friend ostream& operator<<(ostream& os, const RandomArray& ra);
 };
 #endif	// RANDOMARRAY_H
